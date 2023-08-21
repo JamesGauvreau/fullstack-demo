f9fbfcd51be3457e0471ac7786479c2685a5b05d
JamesGauvreau <james.longfrenchname@gmail.com>
2023-08-21 13:48:23 -0700

    very incomplete; back and forth in bRoom today, will return later

--- client/src/components/RegisterForm.jsx
+++ client/src/components/RegisterForm.jsx
@@ -28,9 +28,25 @@ const AuthForm = ({ setToken }) => {
 
   return (
     <>
-      <p>Sign in to see trains!</p>
+      <p>Register below!</p>
       {alert}
       <form onSubmit={handleSubmit}>
+      <label>
+          First Name:
+          <input
+            type="text"
+            value={fName}
+            onChange={(e) => setfName(e.target.value)}
+          />
+        </label>
+        <label>
+          Last Name:
+          <input
+            type="text"
+            value={lName}
+            onChange={(e) => setlName(e.target.value)}
+          />
+        </label>
         <label>
           Username:
           <input
@@ -47,9 +63,17 @@ const AuthForm = ({ setToken }) => {
             onChange={(e) => setPassword(e.target.value)}
           />
         </label>
+        <label>
+          Confirm Password:
+          <input
+            // type="password"
+            // value={password}
+            // onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
         <button type="submit">Submit</button>
       </form>
-      <p>If you don't have an account, then click here to register!</p>
+      {/* <p>If you don't have an account, then click here to register!</p> */}
     </>
   );
 };
